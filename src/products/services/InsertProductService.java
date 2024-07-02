ce8768a6f9414ae1d28944a8c8176146eb7c72b1
emersondantas <me.emerson.dt@gmail.com>
2024-07-02 00:23:26 -0300

    test webhook

--- src/products/services/InsertProductService.java
+++ src/products/services/InsertProductService.java
@@ -12,6 +12,8 @@ public class InsertProductService {
 
     private final ProductRepository productRepository;
 
+    //TODO td(name: Tempo de resposta é lento;)
+
     public Product insertProduct(ProductDTO productDTO) {
         return productRepository.save(Product.from(productDTO));
     }
